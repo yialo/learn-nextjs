56e14c669a09cf6c8fab8b0c96e8cbe8d72f2c32
Aleksei Arro <polsck@gmail.com>
2020-07-10 00:12:39 +0300

    Init new NextJS app

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+export default function IndexPage() {
+  return (
+    <h1>Hello Next.js</h1>
+  );
+}
---
+++ pages/posts.js
@@ -0,0 +1,5 @@
+export default function PostsPage() {
+  return (
+    <h1>Posts</h1>
+  );
+}
