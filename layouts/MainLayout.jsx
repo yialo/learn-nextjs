f9ea931be035d1db2d71663e50e1de6e46f2d748
Aleksei Arro <polsck@gmail.com>
2020-07-12 23:19:51 +0300

    Add module-level styles

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -8,7 +8,6 @@ export function MainLayout({ children }) {
   return (
     <>
       <nav>
-        <p>Navigation</p>
         <ul>
           {LINKS.map((link) => (
             <li key={link}>
@@ -20,6 +19,45 @@ export function MainLayout({ children }) {
         </ul>
       </nav>
       <main>{children}</main>
+
+      <style jsx>{`
+        nav {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 90%;
+          background-color: black;
+        }
+
+        nav > ul {
+          display: flex;
+          justify-content: space-evenly;
+          margin-top: 0;
+          margin-bottom: 0;
+          height: 3rem;
+          padding-left: 0;
+          list-style-type: none;
+        }
+
+        nav > ul > li {
+          display: flex;
+          align-items: center;
+          padding: 0.25rem 0.5rem;
+        }
+
+        nav > ul > li > a {
+          text-decoration: none;
+          color: white;
+        }
+
+        nav > ul > li > a:hover {
+          text-decoration: underline;
+        }
+
+        main {
+          margin-top: 4rem;
+        }
+      `}</style>
     </>
   );
 }
--- pages/posts.jsx
+++ pages/posts.jsx
@@ -1,8 +1,17 @@
+import { useCallback, useState } from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import { MainLayout } from '../layouts/MainLayout';
 
 export default function PostsPage() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = useCallback((event) => {
+    alert(event.value);
+    setInputValue(event.value);
+  }, []);
+
   return (
     <>
       <Head>
@@ -10,6 +19,17 @@ export default function PostsPage() {
       </Head>
       <MainLayout>
         <h1>Posts</h1>
+        <p>
+          <label>Enter post number:</label>
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+        </p>
+        <p>
+          Current input value:
+          <output>{inputValue}</output>
+        </p>
+        <Link href={`/post/${inputValue}`}>
+          <button type="button">Jump!</button>
+        </Link>
       </MainLayout>
     </>
   );
