fa7fb840940a9eb9affe6dd468a1e71cbcb39831
Aleksei Arro <polsck@gmail.com>
2020-07-29 22:16:07 +0300

    Add CSS with modules and globally

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+import style from './MainLayout.module.css';
+
 const HOME_LINK = 'home';
 const LINKS = [HOME_LINK, 'about', 'posts'];
 
@@ -15,57 +17,18 @@ export function MainLayout({ children, title = 'Next App' }) {
         <meta name="keywords" content="javascript, react, nextjs" />
       </Head>
 
-      <nav>
-        <ul>
+      <nav className={style.Menu}>
+        <ul className={style.List}>
           {LINKS.map((link) => (
-            <li key={link}>
+            <li key={link} className={style.Item}>
               <Link href={`/${link === HOME_LINK ? '' : link}`}>
-                <a>{capitalize(link)}</a>
+                <a className={style.Link}>{capitalize(link)}</a>
               </Link>
             </li>
           ))}
         </ul>
       </nav>
-      <main>{children}</main>
-
-      <style jsx>{`
-        nav {
-          position: fixed;
-          top: 0;
-          left: 0;
-          width: 90%;
-          background-color: black;
-        }
-
-        nav > ul {
-          display: flex;
-          justify-content: space-evenly;
-          margin-top: 0;
-          margin-bottom: 0;
-          height: 3rem;
-          padding-left: 0;
-          list-style-type: none;
-        }
-
-        nav > ul > li {
-          display: flex;
-          align-items: center;
-          padding: 0.25rem 0.5rem;
-        }
-
-        nav > ul > li > a {
-          text-decoration: none;
-          color: white;
-        }
-
-        nav > ul > li > a:hover {
-          text-decoration: underline;
-        }
-
-        main {
-          margin-top: 4rem;
-        }
-      `}</style>
+      <main className={style.Main}>{children}</main>
     </>
   );
 }
---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import '../css/styles.css';
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
