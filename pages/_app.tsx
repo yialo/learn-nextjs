accb3b550c1420c4ea81bcb2fd609b79cef5a7b0
Aleksei Arro <polsck@gmail.com>
2020-08-03 16:11:56 +0300

    Add NProgress as navigation indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
 import type { AppProps } from 'next/app';
+import { Router } from 'next/router';
+import NProgress from 'nprogress';
 
+import 'nprogress/nprogress.css';
 import '../css/styles.scss';
 
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+
+Router.events.on('routeChangeError', () => {
+  NProgress.done();
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
